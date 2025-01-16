468919355c419f19096296341c4fd7b93945d794
sandwichdev <tonistuckey8@gmail.com>
2025-01-15 19:33:18 -0500

    fix meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/common/Header";
 import Footer from "@/components/common/Footer";
+import Head from "next/head";
+import Images from "@/utils/images";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,16 +16,11 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-interface ExtendedMetadata extends Metadata {
-  image?: string;
-}
-
-export const metadata: ExtendedMetadata = {
+export const metadata: Metadata = {
   title: "CerkiTek-IT Solutions Simplified",
   description: `CerkiTek helps businesses turn around failing projects, modernize outdated systems, 
       and build custom software tailored to their needs. With a focus on fast recovery, 
       seamless upgrades, and innovative solutions, we ensure your business stays efficient and competitive`,
-  image:'/images/jpg/main.jpg',
 };
 
 export default function RootLayout({
@@ -33,6 +30,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta property="og:image" content={Images.main.src} />
+      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
