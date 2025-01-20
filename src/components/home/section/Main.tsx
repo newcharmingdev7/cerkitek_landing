d85f583e237dea870b7b179826d99e11211e9347
sandwichdev <tonistuckey8@gmail.com>
2025-01-20 15:07:08 -0500

    fix color

--- src/components/home/section/Main.tsx
+++ src/components/home/section/Main.tsx
@@ -8,7 +8,14 @@ const Main = () => {
     <div className="px-8 sm:px-16 lg:px-30 py-4 sm:py-8 lg:py-16 grid grid-cols-1 lg:grid-cols-2 gap-4">
       <div className="w-full flex justify-center items-center">
         <div className="max-w-[650px] grid gap-y-2 lg:max-w-[440px] h-fit">
-          {maincontent.title}
+          <h1 className="text-4xl sm:text-5xl lg:text-6xl font-semibold">
+            Unlock Your <br />{" "}
+            <span className="text-darkRed dark:text-green">
+              Digital Potential
+            </span>{" "}
+            <br />
+            with CerkiTek
+          </h1>
           <p className="text-base text-gray dark:text-white h-fit">
             {maincontent.discription}
           </p>
